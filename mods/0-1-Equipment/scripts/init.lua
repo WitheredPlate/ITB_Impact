b08c8f3bf8124597d85da6a9fdbcd3b13833f215
WitheredPlate <witheredplate@gmail.com>
2026-09-24 23:58:03 -0400

    Added Disintegrator to weapon deck and description

--- mods/0-1-Equipment/scripts/init.lua
+++ mods/0-1-Equipment/scripts/init.lua
@@ -1,7 +1,7 @@
 local mod = {
 	id = "ffrg_impact_equipment",
 	name = "",
-	description = "-- Equipment --\n\nIntroduces new Equipment that can be encountered during runs.\n\nEquipment:\n [Prime]\n  - Gorgon Shredder\n  - Atlas Maul\n [Brute]\n  - Flechette Caster\n  - Hydra Cannonade\n [Ranged]\n  - Horus Drill\n  - Sekhmet Saw\n [Science]\n  - Gravity Manipulator\n  - Heavy Repulsor\n [Support]\n  - Loading Belt\n  - Mounted Armaments\n [Passive]\n  - Hydrogen Fabricators\n",
+	description = "-- Equipment --\n\nIntroduces new Equipment that can be encountered during runs.\n\nEquipment:\n [Prime]\n  - Gorgon Shredder\n  - Disintegrator\n  - Atlas Maul\n [Brute]\n  - Flechette Caster\n  - Hydra Cannonade\n [Ranged]\n  - Horus Drill\n  - Sekhmet Saw\n [Science]\n  - Gravity Manipulator\n  - Heavy Repulsor\n [Support]\n  - Loading Belt\n  - Mounted Armaments\n [Passive]\n  - Hydrogen Fabricators\n",
 	modApiVersion = "2.8.3",
 	version = ffrg_impact_version,
 	gameVersion = "1.2.83",
@@ -13,6 +13,7 @@ function mod:init()
 	require(self.scriptPath .."weapons")
 	modApi:addWeaponDrop("ffrg_Prime_Shred")
 	modApi:addWeaponDrop("ffrg_Prime_Maul")
+	modApi:addWeaponDrop("ffrg_Prime_HeavyLaser")
 	modApi:addWeaponDrop("ffrg_Brute_Cannonade")
 	modApi:addWeaponDrop("ffrg_Brute_Flechette")
 	modApi:addWeaponDrop("ffrg_Ranged_TC_Silica")
